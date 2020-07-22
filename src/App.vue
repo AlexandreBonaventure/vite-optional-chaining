9a341cbbdb241d5ba28020c8c28869d6293291e2
Alexandre Geissmann <alex@la-bonaventure.com>
2020-07-22 09:32:15 -0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld :msg="msg?.test?.foo" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return {
+      msg: {
+        test: null
+      }
+    }
+  }
+}
+</script>
